4628e425b0f9ae53d3cbc8057dd3856b9d3bd508
shiawyonglim <65016718+shiawyonglim@users.noreply.github.com>
2022-02-24 12:10:42 +0800

    Add files via upload

---
+++ python_basic/password_valid.py
@@ -0,0 +1,28 @@
+import re
+
+from sqlalchemy import false
+print('password msut contain 8-20 Letters( Uppercase , Lowercase )and Number')
+password = input('enter your password:')
+factor = True
+
+while factor == True:
+    if (len(password)) >= 8 and (len(password)) <= 20:
+        factor = True
+        if re.search("[a-z]", password):
+            factor = True
+            if re.search("[A-Z]", password):
+                factor = True
+                if re.search("[0-9]", password):
+                    print('')
+                    print('password valid')
+                else:
+                    print('password does not contain numbers')
+            else:
+                print("password does not contain uppercase")
+        else:
+            print('pasword does not contain lowercase letters')
+    elif (len(password)) <= 8:
+        print('password does not have 8 characters')
+    else:
+        print('passwrod is more that 20 characters')
+    factor = False
